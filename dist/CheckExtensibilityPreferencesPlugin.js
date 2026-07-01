a93116e641891f751ac5a38c97df845ed03c2830
Fábio Jorge Neves Domingues <fabio.domingues92@gmail.com>
2026-07-01 14:31:19 +0100

    Update CheckExtensibilityPreferencesPlugin.js

--- dist/CheckExtensibilityPreferencesPlugin.js
+++ dist/CheckExtensibilityPreferencesPlugin.js
@@ -1,10 +1,5 @@
 // Client Action: CheckExtensibilityPreferencesPlugin
 // Output: IsAvailable (Boolean)
 
-$parameters.IsAvailable = false;
-
-if (typeof ExtensibilityPreferences !== 'undefined') {
-    $parameters.IsAvailable = true;
-}
-
+$parameters.IsAvailable = typeof ExtensibilityPreferences !== 'undefined';
 $resolve();
