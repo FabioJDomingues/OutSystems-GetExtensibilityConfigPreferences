169486352a9a7786db18a70af3099b67f3b95902
Fábio Jorge Neves Domingues <fabio.domingues92@gmail.com>
2026-07-01 14:32:29 +0100

    Update GetBooleanPreference.js

--- dist/GetBooleanPreference.js
+++ dist/GetBooleanPreference.js
@@ -2,18 +2,13 @@
 // Input:  Key (Text), DefaultValue (Boolean)
 // Output: Value (Boolean)
 
-$parameters.Value = $parameters.DefaultValue;
-
-if (typeof ExtensibilityPreferences === 'undefined') {
-    $resolve();
-    return;
-}
+let output = false;
 
 ExtensibilityPreferences.getBoolean(
     $parameters.Key,
     $parameters.DefaultValue,
     function (value) {
-        $parameters.Value = value;
+       output = value;
         $resolve();
     },
     function () {
