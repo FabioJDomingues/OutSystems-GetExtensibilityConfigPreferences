1d674c1e84c7ae319318efa55b4b65bd2d7f69be
Fábio Jorge Neves Domingues <fabio.domingues92@gmail.com>
2026-07-01 14:34:07 +0100

    Update GetPreference.js

--- dist/GetPreference.js
+++ dist/GetPreference.js
@@ -1,21 +1,14 @@
 // Client Action: GetPreference
 // Input:  Key (Text)
-// Output: Value (Text), HasValue (Boolean)
+// Output: Value (Text)
 
-$parameters.Value = '';
-$parameters.HasValue = false;
-
-if (typeof ExtensibilityPreferences === 'undefined') {
-    $resolve();
-    return;
-}
+let preferenceValue = '';
 
 ExtensibilityPreferences.getString(
     $parameters.Key,
     function (value) {
         if (value !== null && value !== undefined && value !== '') {
-            $parameters.Value = value;
-            $parameters.HasValue = true;
+            preferenceValue = value;
         }
         $resolve();
     },
