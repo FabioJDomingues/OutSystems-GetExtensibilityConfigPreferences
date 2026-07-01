19deaced878fe826dfb010ceccf4a7fe84bfec11
Fábio Jorge Neves Domingues <fabio.domingues92@gmail.com>
2026-07-01 14:37:12 +0100

    Update output variable to bool in GetBooleanPreference

--- dist/GetBooleanPreference.js
+++ dist/GetBooleanPreference.js
@@ -1,14 +1,13 @@
 // Client Action: GetBooleanPreference
 // Input:  Key (Text), DefaultValue (Boolean)
-// Output: Value (Boolean)
-
-let output = false;
+// Output: bool (Boolean)
+let bool = false;
 
 ExtensibilityPreferences.getBoolean(
     $parameters.Key,
     $parameters.DefaultValue,
     function (value) {
-       output = value;
+       bool = value;
         $resolve();
     },
     function () {
